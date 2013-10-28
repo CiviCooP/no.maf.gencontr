9b78ab86acb8fe685e2a884e87541a0a42435bd0
Erik Hommel <hommel@ee-atwork.nl>
2013-10-28 17:52:54 +0100

    final version, run on maf test

--- CRM/Gencontr/Page/GenRecurr.php
+++ CRM/Gencontr/Page/GenRecurr.php
@@ -1,23 +1,26 @@
 <?php
+ini_set('display_errors', 1);
+set_time_limit(0);
+
 
 require_once 'CRM/Core/Page.php';
 
 class CRM_Gencontr_Page_GenRecurr extends CRM_Core_Page {
     function run() {
         CRM_Utils_System::setTitle(ts('Generating contributions'));
-        require_once 'CRM/Recurring/Form/Lookahead.php';
+        require_once '/home/maf/www/test/sites/civicrm_extensions/no.maf.recurring/Recurring/Form/Lookahead.php';
         
         $selectRecur = "SELECT * FROM civicrm_contribution_recur WHERE contribution_status_id <> 3";
         $daoRecur = CRM_Core_DAO::executeQuery($selectRecur);
         while ($daoRecur->fetch()) {
             $params['cid'] = $daoRecur->contact_id;
-            $params['next_sched_contribution'] = $daoRecur->next_scheduled_contribution;
+            $params['next_sched_contribution'] = $daoRecur->next_sched_contribution;
             $params['end_date'] = $daoRecur->end_date;
             $params['frequency_unit'] = $daoRecur->frequency_unit;
             $params['frequency_interval'] = $daoRecur->frequency_interval;
             $params['amount'] = $daoRecur->amount;
             $params['recur_id'] = $daoRecur->id;
-            CRM_Recurring_Form_Lookahead::contributionCreate();
+            Recurring_Form_Lookahead::contributionCreate($params);
         }
         
         parent::run();
