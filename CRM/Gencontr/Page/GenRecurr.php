a3d5c86e0ee4c36512f96e9e837faeefe1e2c35b
Erik Hommel <hommel@ee-atwork.nl>
2013-10-29 10:52:11 +0100

    version to skip 1 month

--- CRM/Gencontr/Page/GenRecurr.php
+++ CRM/Gencontr/Page/GenRecurr.php
@@ -10,11 +10,23 @@ class CRM_Gencontr_Page_GenRecurr extends CRM_Core_Page {
         CRM_Utils_System::setTitle(ts('Generating contributions'));
         require_once '/home/maf/www/test/sites/civicrm_extensions/no.maf.recurring/Recurring/Form/Lookahead.php';
         
-        $selectRecur = "SELECT * FROM civicrm_contribution_recur WHERE contribution_status_id <> 3";
+        $selectRecur = 
+"SELECT * FROM civicrm_contribution_recur WHERE contribution_status_id <> 3 AND payment_instrument_id = 13";
         $daoRecur = CRM_Core_DAO::executeQuery($selectRecur);
         while ($daoRecur->fetch()) {
+            
+            $nextSchedContribution = $daoRecur->next_sched_contribution;
+            if ($daoRecur->frequence_interval == 1 && $daoRecur->frequency_unit == "month") {
+                $date    = new DateTime($nextSchedContribution);
+                $date->modify(
+                    '+' . $daoRecur->frequency_interval .
+                    ' ' . $daoRecur->frequency_unit
+                );
+                $nextSchedContribution = $date->format('c');
+            }
+            
             $params['cid'] = $daoRecur->contact_id;
-            $params['next_sched_contribution'] = $daoRecur->next_sched_contribution;
+            $params['next_sched_contribution'] = $nextSchedContribution;
             $params['end_date'] = $daoRecur->end_date;
             $params['frequency_unit'] = $daoRecur->frequency_unit;
             $params['frequency_interval'] = $daoRecur->frequency_interval;
